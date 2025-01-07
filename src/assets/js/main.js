a9a779c2b480c986d64ac73e639e2a067f7c074b
SimoneCerri <simone.cerri1994@gmail.com>
2025-01-07 16:18:39 +0100

    change navbar

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import '../style/style.css'
 import App from '../../App.vue'
+/* ### CSS-LINK ### */
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 
--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -18,19 +18,19 @@ export default {
 </script>
 
 <template>
-    <div class="bg-warning row rounded-top-0 rounded-4 pt-4 pb-3 px-5">
+    <!-- <div class="bg-warning row rounded-top-0 rounded-4 pt-4 pb-3 px-5 text-center">
         <div class="col-3">
             <a class="text-decoration-none text-dark" href="#Home">
                 Home
             </a>
         </div>
-        <div class="col-3">
-            <a class="text-decoration-none text-dark ps-1 pe-3 text-nowrap" href="#Home">
+        <div class="col-3 ps-2">
+            <a class="text-decoration-none text-dark text-nowrap" href="#Home">
                 Chi siamo
             </a>
         </div>
         <div class="col-3">
-            <a class="text-decoration-none text-dark px-2" href="#Home">
+            <a class="text-decoration-none text-dark" href="#Home">
                 Servizi
             </a>
         </div>
@@ -39,7 +39,34 @@ export default {
                 Contatti
             </a>
         </div>
-    </div>
+    </div> -->
+
+    <nav class="navbar navbar-expand-lg bg-warning rounded-bottom-4">
+        <div class="container-fluid">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
+                aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarText">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Chi Siamo</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Servizi</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contatti</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+
 </template>
 
 <style></style>
