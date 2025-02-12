f8ecc472541a0c107b21f6f21565c42d74506033
SimoneCerri <simone.cerri1994@gmail.com>
2025-02-12 16:37:22 +0100

    small changes for footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -18,76 +18,84 @@ export default {
 </script>
 
 <template>
-    <div class="container py-5">
-        <div class="row text-center">
-            <div class="col-4">
-                <ul class="list-group rounded-0">
-                    <li class="list-group-item border-0">
-                        Team :
-                    </li>
-                    <li class="list-group-item border-0">
-                        <a class="text-decoration-none" href="https://www.instagram.com/wolly_makeup.art.magic/"
-                            target="_blank" rel="noopener noreferrer">
-                            Project Manager
-                        </a>
-                    </li>
-                    <li class="list-group-item border-0">
-                        <a class="text-decoration-none" href="https://www.behance.net/peach_design" target="_blank"
-                            rel="noopener noreferrer">
-                            Designer
-                        </a>
-                    </li>
-                    <li class="list-group-item border-0">
-                        <a class="text-decoration-none" href="https://github.com/SimoneCerri" target="_blank"
-                            rel="noopener noreferrer">
-                            Developer
-                        </a>
-                    </li>
-                </ul>
+    <section id="footer">
+        <div class="container py-5">
+            <div class="row text-center">
+                <div class="col-4">
+                    <ul class="list-group rounded-0">
+                        <li class="list-group-item border-0">
+                            Legal :
+                        </li>
+                        <li class="list-group-item border-0">
+                            <a class="text-decoration-none" href="" target="_blank" rel="noopener noreferrer">
+                                Privacy Policy
+                            </a>
+                        </li>
+                        <li class="list-group-item border-0">
+                            <a class="text-decoration-none" href="" target="_blank" rel="noopener noreferrer">
+                                Cookies
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-4">
+                    <ul class="list-group rounded-0">
+                        <li class="list-group-item border-0">
+                            Team :
+                        </li>
+                        <li class="list-group-item border-0">
+                            <a class="text-decoration-none" href="https://www.instagram.com/wolly_makeup.art.magic/"
+                                target="_blank" rel="noopener noreferrer">
+                                PM - Wolly
+                            </a>
+                        </li>
+                        <li class="list-group-item border-0">
+                            <a class="text-decoration-none" href="https://www.behance.net/peach_design" target="_blank"
+                                rel="noopener noreferrer">
+                                Design - Peach
+                            </a>
+                        </li>
+                        <li class="list-group-item border-0">
+                            <a class="text-decoration-none" href="https://github.com/SimoneCerri" target="_blank"
+                                rel="noopener noreferrer">
+                                Dev - ScorpioN
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-4">
+                    <ul class="list-group rounded-0">
+                        <li class="list-group-item border-0">
+                            Social :
+                        </li>
+                        <li class="list-group-item border-0">
+                            <a class="text-decoration-none" href="https://www.facebook.com/share/18qC7VoJXt/"
+                                target="_blank" rel="noopener noreferrer">
+                                Facebook
+                            </a>
+                        </li>
+                        <li class="list-group-item border-0">
+                            <a class="text-decoration-none"
+                                href="https://www.instagram.com/dodoconsulente?igsh=MXB0MGpub3A0NHZnbg=="
+                                target="_blank" rel="noopener noreferrer">
+                                Instagram
+                            </a>
+                        </li>
+                    </ul>
+                </div>
             </div>
-            <div class="col-4">
-                <ul class="list-group rounded-0">
-                    <li class="list-group-item border-0">
-                        Legal :
-                    </li>
-                    <li class="list-group-item border-0">
-                        <a class="text-decoration-none" href="" target="_blank" rel="noopener noreferrer">
-                            Privacy Policy
-                        </a>
-                    </li>
-                    <li class="list-group-item border-0">
-                        <a class="text-decoration-none" href="" target="_blank" rel="noopener noreferrer">
-                            Cookies
-                        </a>
-                    </li>
-                </ul>
-            </div>
-            <div class="col-4">
-                <ul class="list-group rounded-0">
-                    <li class="list-group-item border-0">
-                        Social :
-                    </li>
-                    <li class="list-group-item border-0">
-                        <a class="text-decoration-none" href="https://www.facebook.com/share/18qC7VoJXt/"
-                            target="_blank" rel="noopener noreferrer">
-                            Facebook
-                        </a>
-                    </li>
-                    <li class="list-group-item border-0">
-                        <a class="text-decoration-none"
-                            href="https://www.instagram.com/dodoconsulente?igsh=MXB0MGpub3A0NHZnbg==" target="_blank"
-                            rel="noopener noreferrer">
-                            Instagram
-                        </a>
-                    </li>
-                </ul>
+            <hr>
+            <div class="row text-center">
+                <div class="col-12">
+                    Made by ScorpioN
+                </div>
             </div>
         </div>
-    </div>
+    </section>
 </template>
 
 <style scoped>
-*,
+#footer,
 .list-group-item {
     background-color: var(--strong-color);
 }
